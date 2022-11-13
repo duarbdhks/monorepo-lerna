78c88da9dc1541f30228840f96bcc603c9db779b
Yeum Gu Whan <guwhan.yeum@flitto.com>
2022-11-13 18:55:10 +0900

    refactor: 사용하지 않는 import 제거

--- packages/s3/lib/s3.js
+++ packages/s3/lib/s3.js
@@ -1,10 +1,13 @@
 'use strict';
-const { test, retryAPI } = require('@duarbdhks/request')
+const { retryAPI } = require('@duarbdhks/request')
 
 exports.init = async (config) => {
   console.log(config, 'duarbdhks 111111111')
   const content = 'Hello from request'
-  const result = await retryAPI({ url: `https://www.flitto.com/api/1.2/util/detect/text?content=${content}`, method: 'GET' })
+  const result = await retryAPI({
+    url: `https://www.flitto.com/api/1.2/util/detect/text?content=${content}`,
+    method: 'GET'
+  })
   console.log(result, 'duarbdhks 2222222')
   return "Hello from s3";
 }
