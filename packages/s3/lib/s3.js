73d42e6787c1ce9d72aa80b49e41b5fe6fe8eb99
Yeum Gu Whan <guwhan.yeum@flitto.com>
2022-11-13 19:30:03 +0900

    refactor: 필요없는 콘솔제거

--- packages/s3/lib/s3.js
+++ packages/s3/lib/s3.js
@@ -11,13 +11,11 @@ const {
 } = require('@aws-sdk/client-s3')
 
 exports.init = async (config) => {
-  console.log(config, 'duarbdhks 111111111')
   const content = 'Hello from request'
   const result = await retryAPI({
     url: `https://www.flitto.com/api/1.2/util/detect/text?content=${content}`,
     method: 'GET'
   })
-  console.log(result, 'duarbdhks 2222222')
   return "Hello from s3";
 }
 
