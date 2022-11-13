1306d2577c7da2dc876c88cb67e86dc1e9c22034
Yeum Gu Whan <guwhan.yeum@flitto.com>
2022-11-13 18:38:30 +0900

    adsfadsf: 2312123

--- packages/s3/lib/s3.js
+++ packages/s3/lib/s3.js
@@ -3,9 +3,8 @@ const { test, retryAPI } = require('@duarbdhks/request')
 
 exports.init = async (config) => {
   console.log(config, 'duarbdhks 111111111')
-  console.log(test(),'duarbdhks 222222222')
   const content = 'Hello from request'
   const result = await retryAPI({ url: `https://www.flitto.com/api/1.2/util/detect/text?content=${content}`, method: 'GET' })
-  console.log(result, 'duarbdhks 333333333')
+  console.log(result, 'duarbdhks 2222222')
   return "Hello from s3";
 }
